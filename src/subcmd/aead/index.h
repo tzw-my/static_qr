abe3471d971d44ca722197f9a24d3077a27df9c8
jzy-w <tangzhiwen@x-cmd.com>
2023-05-12 15:57:54 +0800

    fix bug

--- src/subcmd/aead/index.h
+++
@@ -1,2 +0,0 @@
-#include "xchacha20ploy1305.h"
-
--- src/util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct binary_data {
-    size_t len;
-    const unsigned char* data;
-};
-
-char* base64encode( struct binary_data bin );
-struct binary_data base64decode(const char* text);
