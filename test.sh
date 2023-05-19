6d61c8484dc4527332cf6406007d16714b049d19
jzy-w <tangzhiwen@x-cmd.com>
2023-05-19 11:06:44 +0800

    update

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 x log init pkg_test
 git clone https://github.com/x-cmd/pkg.git
 x pkg update
+printf "%s\n" "| 软件名 | 软件运行path | 异常 | 错误信息 |" >> ./ret.md
 ls pkg/src/osman | for pkg_bin in $(cat); do
   url="$( ___x_cmd_pkg___list "${pkg_bin}" "v0.0.0" "$os_name" "url.internet" )"
   pkg_test:info "========================${pkg_bin}===================="
@@ -10,11 +11,11 @@ ls pkg/src/osman | for pkg_bin in $(cat); do
 
   bin_name="$(___x_cmd_pkg___list "${pkg_bin}" "v0.0.0" "$os_name" "xbin.${pkg_bin}")"
   [ -z "$bin_name" ] && bin_name="$(___x_cmd_pkg___list "${pkg_bin}" "v0.0.0" "$os_name" "xbin" | { read -r line ; printf "%s\n" "$line" ;} )"
-  bin_path="$(find ./${pkg_bin}_dir -name "${bin_name##*/}")"
+  bin_path="$(find ./${pkg_bin}_dir -name "${bin_name##*/}" -type f )"
   chmod +x "$bin_path"
-  $bin_path --help >/dev/null || {
+  $bin_path --help 1>/dev/null 2>log || {
     pkg_test:error "bin test failed: ${bin_name##*/},path : $bin_path"
-    printf "%s\n" "| ${bin_name} | ❌ |" >> ./ret.md
+    printf "%s\n" "| ${pkg_bin} | ${bin_path} | ❌ | $(cat log) |" >> ./ret.md
   }
 done
 cat ./ret.md
